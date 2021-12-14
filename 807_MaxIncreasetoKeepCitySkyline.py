3a18af70178c43169da783d3cc71b5fbfae2e651
VanDeBiao <1306365660@qq.com>
2021-12-14 15:50:22 +0800

    mostly Leetcode problems solved, commited and pushed by bat

---
+++ 807_MaxIncreasetoKeepCitySkyline.py
@@ -0,0 +1,44 @@
+# https://leetcode.com/problems/max-increase-to-keep-city-skyline/submissions/
+
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        # # v1 dumb 72ms too much useless steps
+        # SouthView=list(map(max,zip(*grid)))
+        # WesthView=list(map(max,grid))
+        # print(SouthView)
+        # print(WesthView)
+        # NScapacity=[]
+        # l=len(grid)
+        # for i in range(l):
+        #     NScapacity.append(SouthView.copy())
+        # sum=0
+        # for i in range(l):
+        #     for j in range(l):
+        #         if NScapacity[i][j]>WesthView[i]:
+        #             NScapacity[i][j]=WesthView[i]
+        #         sum+=NScapacity[i][j]-grid[i][j]
+        # print(NScapacity)
+        # return sum
+
+        # # v2 slow 840ms, using zip in the loop slowed down the whole code
+        # l=len(grid)
+        # sum=0
+        # for i in range(l):
+        #     for j in range(l):
+        #         maxheight=min(max(grid[i]),max(list(zip(*grid))[j]))
+        #         if grid[i][j]<maxheight:
+        #             sum+=maxheight-grid[i][j]
+        # return sum
+
+        # v2.1 faster, 68ms, just zip once 
+        l=len(grid)
+        sum=0
+        SouthView=list(map(max,zip(*grid)))
+        WesthView=list(map(max,grid))
+        for i in range(l):
+            for j in range(l):
+                maxheight=min(SouthView[j],WesthView[i])
+                if grid[i][j]<maxheight:
+                    sum+=maxheight-grid[i][j]
+        return sum
+
---
+++ Unfinished_416_PartitionEqualSubsetSum.py
@@ -0,0 +1,18 @@
+
+# Knapsack(Backpack) prblems, guide:
+# https://leetcode.com/discuss/study-guide/1200320/Thief-with-a-knapsack-a-series-of-crimes.
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        for sublen in range(1,len(nums)//2+1):
+            subset=[0]*sublen
+            for subitemindex in range(sublen):
+
+                subset[subitemindex]=nums[]
+
+
+
+
+                # for i in range(subitemindex,sublen+1):
+                #     subset.append(nums[i])
+                
