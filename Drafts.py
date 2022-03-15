0cfbcda256cd1012280b95ee16eff0ac25e9d5d9
Colt-M1873 <47551017+Colt-M1873@users.noreply.github.com>
2022-03-15 12:15:55 +0800

    synced and added some books

--- Drafts.py
+++ Drafts.py
@@ -1,76 +1,77 @@
-# # # # # # # list1 = [1, 2, 3, 5, 1, 85, 1, 0]
-# # # # # # # result1 = [None for i in list1]
-# # # # # # # # result2 = [None]*len(list1)
+# # # # # # # # list1 = [1, 2, 3, 5, 1, 85, 1, 0]
+# # # # # # # # result1 = [None for i in list1]
+# # # # # # # # # result2 = [None]*len(list1)
 
-# # # # # # # # print(x[::-2])
-# # # # # # # print(result1)
+# # # # # # # # # print(x[::-2])
+# # # # # # # # print(result1)
 
-# # # # # # # lrcount=[0,1,2,3,4,5]
-# # # # # # # print(lrcount[3:6])
+# # # # # # # # lrcount=[0,1,2,3,4,5]
+# # # # # # # # print(lrcount[3:6])
 
-# # # # # # # s='[([])]'
-# # # # # # # print(s[1:5])
+# # # # # # # # s='[([])]'
+# # # # # # # # print(s[1:5])
 
-# # # # # # # for i in range(3):
-# # # # # # #     print(i)
+# # # # # # # # for i in range(3):
+# # # # # # # #     print(i)
 
-# # # # # # a=[0,8,1,0,2,3,4,15,41,894,0]
-# # # # # # i=0
-# # # # # # n=0
-# # # # # # # while i < len(a)-n:
-# # # # # # #     if a[i]==0:
-# # # # # # #         a.pop(i)
-# # # # # # #         a.append(0)
-# # # # # # #         n+=1
-# # # # # # #     else:
-# # # # # # #         i+=1
+# # # # # # # a=[0,8,1,0,2,3,4,15,41,894,0]
+# # # # # # # i=0
+# # # # # # # n=0
+# # # # # # # # while i < len(a)-n:
+# # # # # # # #     if a[i]==0:
+# # # # # # # #         a.pop(i)
+# # # # # # # #         a.append(0)
+# # # # # # # #         n+=1
+# # # # # # # #     else:
+# # # # # # # #         i+=1
 
-# # # # # # # a.sort(key= )
+# # # # # # # # a.sort(key= )
 
-# # # # # # lb=lambda x : 1 if x==1 else 0
-# # # # # # print(lb(1))
-# # # # # # a.sort(key= lambda x: 1 if x == 0 else 0)
+# # # # # # # lb=lambda x : 1 if x==1 else 0
+# # # # # # # print(lb(1))
+# # # # # # # a.sort(key= lambda x: 1 if x == 0 else 0)
 
-# # # # # # import random
-# # # # # # a.sort(key= random.randint(0,a[5]))
+# # # # # # # import random
+# # # # # # # a.sort(key= random.randint(0,a[5]))
 
-# # # # # # print(a)
-# # # # # # print(not None)
+# # # # # # # print(a)
+# # # # # # # print(not None)
 
 
-# # # # # s='ibhusadgzdfzwef'
-# # # # # b='ibh'
-# # # # # s=''
-# # # # # b='dfs3'
-# # # # # print(s.find(b))
+# # # # # # s='ibhusadgzdfzwef'
+# # # # # # b='ibh'
+# # # # # # s=''
+# # # # # # b='dfs3'
+# # # # # # print(s.find(b))
 
-# # # # for i in range(10,1,-1):
-# # # #     print(i)
+# # # # # for i in range(10,1,-1):
+# # # # #     print(i)
 
-# # # # a=[1,2,3,4]
-# # # # a.insert(0,91873)
-# # # # print(a)
-# # # # b=list(map(int, str(int(''.join(map(str,a)))+1)))
-# # # # print(b)
-# # # # print(type(b))
+# # # # # a=[1,2,3,4]
+# # # # # a.insert(0,91873)
+# # # # # print(a)
+# # # # # b=list(map(int, str(int(''.join(map(str,a)))+1)))
+# # # # # print(b)
+# # # # # print(type(b))
 
-# # # a='9'
-# # # b='1'
-# # # print(str(bin(int(a)))[2:])
+# # # # a='9'
+# # # # b='1'
+# # # # print(str(bin(int(a)))[2:])
 
-# # fact=1
-# # for i in range(6,6):
-# #     fact*=i
-# # print(fact)
+# # # fact=1
+# # # for i in range(6,6):
+# # #     fact*=i
+# # # print(fact)
 
-# a=[5424,12]
-# b=[-1800]
+# # a=[5424,12]
+# # b=[-1800]
 
-# print(a.pop(0))
-# print(a)
-# if a and b:
-#     print(1)
-a=[]
-for i in range(5):
-    a.append(i)
-print(a[4:5])
+# # print(a.pop(0))
+# # print(a)
+# # if a and b:
+# #     print(1)
+# a=[]
+# for i in range(5):
+#     a.append(i)
+# print(a[4:5])
+import this
