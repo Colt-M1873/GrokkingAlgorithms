14a78c40deb773eeece9ee71fd399d48e117216e
VanDeBiao <1306365660@qq.com>
2022-04-17 11:27:21 +0800

    mostly Leetcode problems solved, commited and pushed by bat

---
+++ LeetCode/4.16.py
@@ -0,0 +1,82 @@
+
+row=0
+col=0
+nums=[]
+
+def right(n,m):
+    global row,col
+    while col<m-2:
+        col+=2
+        if nums[row][col]=='*':
+
+            nums[row][col]='.'
+        else:
+            break
+
+def down(n,m):
+    global row,col
+    while row<n-2:
+        row+=2
+        if nums[row][col]=='*':
+            nums[row][col]='.'
+        else:
+            break
+
+def left(n,m):
+    global row,col
+    while col>1:
+        col-=2
+        if nums[row][col]=='*':
+            nums[row][col]='.'
+        else:
+            break
+
+def up(n,m):
+    global row,col
+    while row>1:
+        row-=2
+        if nums[row][col]=='*':
+            nums[row][col]='.'
+        else:
+            break
+
+def main():
+    n=3
+    m=3 # 处理输入
+    global nums
+    nums1=["**.",".*.","***"]
+    nums=[]
+
+    for item in nums1:
+        nums.append(list(item))
+
+    global row,col
+    result =0
+    for i in range(n):
+        for j in range(m):
+            if nums[i][j]=='*':
+                nums[row][col]='.'
+                row=i
+                col=j
+                result+=1
+                right(n,m)
+                down(n,m)
+                left(n,m)
+                up(n,m)
+                right(n,m)
+                down(n,m)
+                left(n,m)
+                up(n,m)
+                
+    print(result)
+
+    return 
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
