1bec2d33f5a3dffe417632e3373fab6dd5f71a1c
CyberDJ <47551017+Colt-M1873@users.noreply.github.com>
2023-08-24 14:42:15 +0800

    Update 509_FibonacciNumber.java

--- LeetCode/509_FibonacciNumber.java
+++ LeetCode/509_FibonacciNumber.java
@@ -14,3 +14,34 @@ class Solution {
         return c;
     }
 }
+
+###### 2023年08月24日 14:37:08
+
+class Solution {
+    public int fib(int n) {
+        // // recursive 6ms
+        // if(n<2){return n;}
+        // return fib(n-1)+fib(n-2);
+        
+        // dp 0ms
+        if(n<2){return n;}
+        int[] a = new int[n+1];
+        a[1]=1;
+        for(int i=2;i<n+1;i++){
+            a[i]=a[i-1]+a[i-2];
+        }
+        return a[n];
+
+        // O1 space dp
+        if(n<2){return n;}
+        int a,b,c;
+        a=0;b=1;c=1;
+        for(int i=2;i<n+1;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
+    }
+}
+
