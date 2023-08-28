fe9d26f428edbfbc1562353f7702f51f1428e299
CyberDJ <47551017+Colt-M1873@users.noreply.github.com>
2023-08-28 17:05:15 +0800

    Update 102_BinaryTreeLevelOrderTraversal.java

--- LeetCode/102_BinaryTreeLevelOrderTraversal.java
+++ LeetCode/102_BinaryTreeLevelOrderTraversal.java
@@ -62,19 +62,19 @@ class Solution {
         // Queue<TreeNode> queue = new LinkedList<>();  
         // queue.add(root);  
         // while (!queue.isEmpty()) {  
-        // 　　List<Integer> level = new ArrayList<>();  
-        // 　　int cnt = queue.size();  
-        // 　　for (int i = 0; i < cnt; i++) {  
-        // 　　　　TreeNode node = queue.poll();  
-        // 　　　　level.add(node.val);  
-        // 　　　　if (node.left != null) {  
-        // 　　　　　　queue.add(node.left);  
-        // 　　　　}
-        // 　　　　if (node.right != null) {  
-        // 　　　　　　queue.add(node.right);  
-        // 　　　　}  
-        // 　　}  
-        // 　　res.add(level);   
+        //     List<Integer> level = new ArrayList<>();  
+        //     int cnt = queue.size();  
+        //     for (int i = 0; i < cnt; i++) {  
+        //         TreeNode node = queue.poll();  
+        //         level.add(node.val);  
+        //         if (node.left != null) {  
+        //             queue.add(node.left);  
+        //         }
+        //         if (node.right != null) {  
+        //             queue.add(node.right);  
+        //         }  
+        //     }  
+        //     res.add(level);   
         // }  
         // return res;
 
