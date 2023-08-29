6698a5a3d07878c0903f5ca42a9ae5915d710011
CyberDJ <47551017+Colt-M1873@users.noreply.github.com>
2023-08-29 10:12:43 +0800

    Update 198_HouseRobber.java

--- LeetCode/198_HouseRobber.java
+++ LeetCode/198_HouseRobber.java
@@ -49,3 +49,24 @@ class Solution {
         return Math.max(nums[nums.length - 1], nums[nums.length - 2]);
     }
 }
+
+
+// 2023年08月29日 10:12:26
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1){return nums[0];}
+        nums[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            nums[i]=Math.max(nums[i-2]+nums[i],nums[i-1]);
+        }
+        return nums[nums.length-1];
+    }
+}
+
+
+
+
+
+
+
+
