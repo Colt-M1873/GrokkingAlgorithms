3f859da0a5f3a95547c47e06a18474c5068763a4
CyberDJ <47551017+Colt-M1873@users.noreply.github.com>
2024-01-23 21:45:51 +0800

    Create 222._CountCompleteTreeNodes.java

---
+++ LeetCode/222._CountCompleteTreeNodes.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/count-complete-tree-nodes/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root==null){return 0;}
+        return this.countNodes(root.left)+1+this.countNodes(root.right);
+    }
+}
